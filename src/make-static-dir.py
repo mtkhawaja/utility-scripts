0d47a58d01b3d051957e2101194aaba221e0242f
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-10-18 00:35:25 -0400

    Updated make-static-dir.py to include an app name. i.e. static/appname/ instead of static/ as the parent directory.

--- src/make-static-dir.py
+++ src/make-static-dir.py
@@ -7,9 +7,9 @@ parser = ArgumentParser()
 
 def create_static_dir(args):
     parent_dir_path = args.target_path
-    dir = 'static'
+    dir = f'static/{args.app_name}'
     dir_path = join(parent_dir_path, dir)
-    mkdir(dir_path)
+    makedirs(dir_path)
     subfolder_level_one = ['scripts/js/','styles/css/']
     for subfolders in subfolder_level_one:
         path = join(dir_path, subfolders)
@@ -18,6 +18,7 @@ def create_static_dir(args):
 
 
 parser.add_argument('-t', '--target_path', default='./', help='Target Destination for Static Directory')
+parser.add_argument('-a', '--app_name', required=True, help='Name of the application for the static directory.')
 parser.set_defaults(func=create_static_dir)
 args = parser.parse_args()
 args.func(args)
